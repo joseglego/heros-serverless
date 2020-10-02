05f70800801117e1fbed4d8fc84f30c5f369c0c2
Jose Lezama <joseglego@gmail.com>
2020-10-02 11:05:49 -0300

    feat(): get groups by heroId

--- functions/groups.js
+++ functions/groups.js
@@ -1,8 +1,17 @@
+const heros = require('./utils/heros.json');
 const groups = require('./utils/groups.json');
 
 exports.handler = async ({ queryStringParameters }) => {
+  const heroId = +queryStringParameters.heroId;
+  let selectedGroups = groups;
+
+  if (heroId) {
+    const hero = heros.find(hero => hero.id === heroId);
+    selectedGroups = selectedGroups.filter(group => hero.publisher === group.name);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify(groups)
+    body: JSON.stringify(selectedGroups)
   };
 };
